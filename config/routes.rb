581ac109f40ed1e590fe67cc1144ff5e85cce505
Takeru <takeru.24.sgw@gmail.com>
2020-05-26 07:31:33 +0000

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/new'
   root 'static_pages#index'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
 end
